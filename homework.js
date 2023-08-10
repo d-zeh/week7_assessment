3a3eb7c6dc31cd581fb8e99cbcb03cf7017ec107
Daniel Zeh <dfzeh1@gmail.com>
2023-08-10 02:26:00 -0500

    q

--- homework.js
+++ homework.js
@@ -3,13 +3,13 @@
 function addtoZero(array) {
     if ((array.length == 1) || array.length == 0) {
         console.log("false")
-        break
+        return
     }
     let (i = 0, i <array.length -1, i++) {
         let (j = i+1, j <array.length, j++) {
             if (array[i] + array[j] == 0) {
                 console.log("false")
-                break
+                return
             }
         }
     }
