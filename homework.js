2008fb523e54371a5b235212901159765aae5690
Daniel Zeh <dfzeh1@gmail.com>
2023-08-10 02:49:51 -0500

    2

--- homework.js
+++ homework.js
@@ -17,13 +17,45 @@ function addtoZero(array) {
     return
 
 }
-
 addtoZero([2,3,4]);
-
 addtoZero([]);
-
 addtoZero([1]);
-
 addtoZero([1, 2, 3]);
-
 addtoZero([1, 2, 3, -2]);
+
+//Unique Characters
+function hasUniqueChars(word) {
+    for (let i = 0; i < word.length; i++) {
+        for (let j = 1; j < word.length; j++) {
+            if (i !== j) {
+                if (word[i] === word[j]) {
+                    console.log("false")
+                    return false
+                }
+            }
+        }
+    }
+    console.log(console.log("true"))
+    return true
+}
+hasUniqueChars("Monday");
+hasUniqueChars("Moonday")
+
+
+//Pangram
+
+function isPangram(input) {
+    let sentence = input.toLowerCase();
+    let letters = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+    for (let i = 0; i < letters.length; i++) {
+        if(sentence.indexOf(letters[i]) < 0) {
+            console.log("false");
+            return false;
+        }
+    }
+    console.log("true")
+    return true
+}
+isPangram("The quick brown fox jumps over the lazy dog!");
+isPangram("I like cats, but not mice");
