2311524f43ada54845e203d918a1ddc0f13c27a5
Daniel Zeh <dfzeh1@gmail.com>
2023-08-10 02:21:24 -0500

    first fun

--- homework.js
+++ homework.js
@@ -3,7 +3,16 @@
 function addtoZero(array) {
     if ((array.length == 1) || array.length == 0) {
         console.log("false")
+        break
+    }
+    let (i = 0, i <array.length -1, i++) {
+        let (j = i+1, j <array.length, j++) {
+            if (array[i] + array[j] == 0) {
+                console.log("false")
+                break
+            }
+        }
     }
 }
 
-addtoZero([])
+addtoZero([2,3,4])
